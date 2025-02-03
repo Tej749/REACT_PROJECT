2f30c83f5ab2d70175b64007ea4d6c43d4691caa
Tej749 <jargha261975@gmail.com>
2025-02-03 07:00:53 +0545

    React Project connect with Express (Backend)..

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,23 +1,30 @@
-import Card from "./components/Card"
-import Navbar from "./components/Navbar"
+import { useEffect, useState } from "react";
+import Card from "./components/Card";
+import Navbar from "./components/Navbar";
+import axios from "axios";
 
-function Home(){
-    return (
-        <>
-            <Navbar />
-            <div className="flex flex-wrap">
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-
-
-            </div>
-           
-        
-
-        </>
-    )
+function Home() {
+  const [blogs, setBlogs] = useState([]);
+  const fetchBlogs = async () => {
+    const response = await axios.get("http://localhost:3000/blog");
+    // console.log(response.data.data)
+    setBlogs(response.data.data);
+  };
+  useEffect(() => {
+    fetchBlogs();
+  }, []);
+  console.log(blogs);
+  return (
+    <>
+      <Navbar />
+      <div className="flex flex-wrap">
+        {blogs.map(function (blog) {
+          return <Card blog={blog} />;
+        })}
+      </div>
+    </>
+  );
 }
 
-export default Home
+export default Home;
+ 
